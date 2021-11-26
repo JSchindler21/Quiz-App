c5a83eca75c41da0c59306aa8446a6a1db289a31
Janet Schindler <nic07_schindler@web.de>
2021-11-26 09:28:06 +0100

    changed

--- src/button.js
+++ src/button.js
@@ -1,11 +1,24 @@
-
 const buttonsQuestion = document.querySelectorAll('.question_button');
+const buttonsAnswers = document.querySelectorAll('.answer_button');
 
-const buttonAnswers = document.querySelectorAll('.answer_button');
-
+ 
 
 buttonsQuestion.forEach((buttonsQuestion, index) => {
-  buttonsQuestion.addEventListener('click', () => {
-    buttonAnswers[index].classList.toggle('hide');
+    buttonsQuestion.addEventListener('click', () => {
+    buttonsAnswers[index].classList.toggle('hide')
+   
+    
   });
 }); 
+
+
+const bookmarks = document.querySelectorAll('question_marks')
+
+bookmarks.forEach(bookmarks => {
+  
+  bookmarks.addEventlistner('cklick', () => {
+      bookmarks.classList.toggle('active')
+      bookmarks.classList.toggle('inactive')
+    
+  })
+})
